751ba603046b6f1d05afc9943d1820f5ebfdd884
Star Nikto <star.nikto@gmail.com>
2025-09-16 11:11:30 +0300

    add custom_swagger_ui_html

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.openapi.docs import get_swagger_ui_html
 import uvicorn
 
 app = FastAPI(docs_url=None)
@@ -8,6 +9,16 @@ app = FastAPI(docs_url=None)
 def func():
     return "Hello World!!!!!!!!!!"
 
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
+        swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
+    )
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
