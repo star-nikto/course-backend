4d2a18fb9c46952af036c44c2730315435dfcf9f
Star Nikto <star.nikto@gmail.com>
2025-09-16 11:58:12 +0300

    add openapi_examples for POST /hotels

--- hotels.py
+++ hotels.py
@@ -1,4 +1,4 @@
-from fastapi import Query, APIRouter
+from fastapi import Query, APIRouter, Body
 
 from schemas.hotels import Hotel, HotelPATCH
 
@@ -26,7 +26,23 @@ def get_hotels(
 
 
 @router.post("")
-def create_hotel(hotel_data: Hotel):
+def create_hotel(hotel_data: Hotel = Body(openapi_examples={
+    "1": {
+        "summary": "Сочи",
+        "value": {
+            "title": "Отель Сочи 5 звезд у моря",
+            "name": "sochi_u_morya",
+        }
+    },
+    "2": {
+        "summary": "Дубай",
+        "value": {
+            "title": "Отель Дубай У фонтана",
+            "name": "dubai_fountain",
+        }
+    }
+})
+):
     global hotels
     hotels.append({
         "id": hotels[-1]["id"] + 1,
