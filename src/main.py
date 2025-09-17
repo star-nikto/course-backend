f36eb632cebe3506b42058861b94953ad26fb777
Star Nikto <star.nikto@gmail.com>
2025-09-17 12:32:48 +0300

    create src/ folder

--- src/main.py
+++ src/main.py
@@ -2,7 +2,12 @@ from fastapi import FastAPI
 from fastapi.openapi.docs import get_swagger_ui_html
 import uvicorn
 
-from hotels import router as router_hotels
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent))
+
+from src.api.hotels import router as router_hotels
 
 app = FastAPI(docs_url=None)
 
