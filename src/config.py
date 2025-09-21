0b0c843510f4aca946def7f6bebbcf558e8bb91d
Star Nikto <star.nikto@gmail.com>
2025-09-21 18:09:58 +0300

    add HotelsOrm & class Base 2

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
@@ -11,6 +11,8 @@ class Settings(BaseSettings):
     @property
     def DB_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
